940c36fe9c6c6c8cb39e784834ecb754b95f3d1b
Jordan Graham <jograham77@gmail.com>
2018-10-06 15:50:54 -0400

    Added the namechanger javascript file
    -This file will be deleted after it is incorporated

---
+++ static/namechanger.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $("#getHello").on("click", function(){
+      // Only change code below this line.
+      
+      
+      // Only change code above this line.
+    });
+
+  });
