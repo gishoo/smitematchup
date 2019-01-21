83f421e44e64cd96cfe7d2d0286f2492734c380c
Jordan Graham <jograham77@gmail.com>
2019-01-21 02:20:57 -0500

    changes in app.py
    Finished load_char function in app.py
    -simplified the routine
    -now the batch_num determines the number of columns displayed

--- app.py
+++ app.py
@@ -32,32 +32,26 @@ def create_app():
       char_list = ['Achilles','Bellona','Guan Yu','Mercury','Sol','Agni','Cabrakan','Hachiman','Neith','Sun Wukong','Ah Muzen Cab','Camazotz','Hades','Nemesis','Susano','Ah Puch','Cerberus','He Bo','Ne Zha','Sylvanus','Amaterasu','Cernunnos','Hel','Nike','Terra','Anhur','Chaac','Hercules','Nox','Thanatos','Anubis',"Chang'e",'Hou Yi','Nu Wa','The Morrigan','Ao Kuang','Chiron','Hun Batz','Thor','Aphrodite','Chronos','Isis','Osiris','Thoth','Apollo','Cu Chulainn','Izanami','Poseidon','Tyr','Arachne','Cupid','Janus','Ra','Ullr','Ares','Da Ji','Jing Wei','Raijin','Vamana','Artemis','Discordia','Kali','Rama','Vulcan','Artio','Erlang Shen','Khepri','Ratatoskr','Xbalanque','Athena','Fafnir','Kukulkan','Ravana','Xing Tian','Awilix','Fenrir','Kumbhakarna','Scylla','Ymir','Bacchus','Freya','Kuzenbo','Serqet','Zeus','Bakasura','Ganesha','Loki','Skadi','Zhong Kui','Bastet','Geb','Medusa','Sobek']
       char_list.sort()
 
-      #This is used to define how many cards to show per page
+      #num_chars should get its value from the num of records in the database change batch_num aswell
+      #batches is the number of columns that will appear on the character list page
       num_chars = len(char_list)
-      batch_num = 10
-      last_index = num_chars % batch_num
-      batch_char_list = []
+      batches = 7
+      batch_num = num_chars // batches
+      batch_list = []
 
-      #divides the char list into sub lists and adds it to a list of lists
-      count = 0
-      total_count = 0
-      curr_list = []
-
-      for curr_char in char_list:
-         if count == batch_num:
-            batch_char_list.append(curr_list)
-            count = 0
-            curr_list = []
-         elif total_count == num_chars-1:
-            curr_list.append(curr_char)
-            batch_char_list.append(curr_list)
-         else:
-            curr_list.append(curr_char)
-            count += 1
-         total_count += 1
+      for num in range(batches):
+         tmp_list = []
+         for count in range(batch_num+1):
+            tmp = num + count * batches
+            if tmp <= num_chars-1:
+               tmp_list.append(char_list[tmp])
+            else:
+               print("\n\n\n",tmp,"\n\n\n")
+         batch_list.append(tmp_list)
+      print("\n",batch_list,"\n")
 
       #fill dict with the variables needed for the templates
-      var_dict = {"char":char, "batch_list":batch_char_list }
+      var_dict = {"char":char, "batch_list":batch_list }
       return render_template("char_build.html", **var_dict)
 
 
